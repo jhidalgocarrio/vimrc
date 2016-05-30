9959a421829a2d3e97b120209307853e56701462
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2016-05-30 19:09:26 +0200

    config ycm py file

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -11,9 +11,32 @@ default_flags = [
 # to be added to _all_ returned flags, always
 mandatory_flags = [
     '-Wall',
+    '-Wextra',
+    '-Werror',
     '-Wpedantic',
-    '-x','c++',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-DUSE_CLANG_COMPLETER',
+    # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
+    # language to use when compiling headers. So it will guess. Badly. So C++
+    # headers will be compiled as C headers. You don't want that so ALWAYS specify
+    # a "-std=<something>".
+    # For a C project, you would set this to something like 'c99' instead of
+    # 'c++11'.
     '-std=c++11',
+    # ...and the same thing goes for the magic -x option which specifies the
+    # language that the files to be compiled are written in. This is mostly
+    # relevant for c++ headers.
+    # For a C project, you would set this to 'c' instead of 'c++'.
+    '-x', 'c++',
+    # This path will only work on OS X, but extra paths that don't exist are not
+    # harmful
+    '-isystem', '/System/Library/Frameworks/Python.framework/Headers',
+    '-isystem', '/usr/local/include',
+    '-isystem', '/usr/local/include/eigen3',
+    '-I', 'include',
+    '-I.',
 ]
 
 # flags which should never be forwarded to clang (if they are not supported for
